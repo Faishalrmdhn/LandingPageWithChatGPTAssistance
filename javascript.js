ca60399767c7ca3a8bdb32ce3231b35d01930d3f
Faishalrmdhn <faishalramadhan25@gmail.com>
2023-01-11 16:59:08 +0700

    fix navbar, add css curve, change bg image

--- javascript.js
+++ javascript.js
@@ -1,9 +1,31 @@
-function scrollToOptions(id) {
-    // ambil elemen yang ingin dituju
-    const element = document.querySelector(id);
+// select all links in the nav
+const navList = document.querySelectorAll('.nav-list a');
+const navBrand = document.querySelectorAll('.nav-brand a');
 
-    // scroll ke elemen tersebut dengan transisi
-    element.scrollIntoView({
+// add a click event to each link
+navList.forEach(link => {
+  link.addEventListener('click', event => {
+    event.preventDefault();
+    // get the id of the section we want to scroll to
+    const sectionId = event.target.getAttribute('href');
+    // select the section
+    const section = document.querySelector(sectionId);
+    // calculate the distance from the top of the section to the top of the page
+    const offsetTop = section.offsetTop;
+    // use javascript to scroll to the section
+    window.scrollTo({
+      top: offsetTop,
       behavior: 'smooth'
     });
-  }
+  });
+});
+
+
+function setTo(id) {
+  const section = document.querySelector(id);
+    const offsetTop = section.offsetTop;
+    window.scrollTo({
+      top: offsetTop,
+      behavior: 'smooth'
+    });
+}
